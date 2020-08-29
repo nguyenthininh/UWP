ff92e9b521451c462a0f9af36973830edb20cf6a
Nina Nguyen <Ninh2000md>
2020-08-29 18:51:03 +0700

    exam

---
+++ Exam_UWP/MainPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using Exam_UWP.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using static Exam_UWP.Models.Product;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Exam_UWP
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private ObservableCollection<Product> Products;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            Products = new ObservableCollection<Product>();
+            ProductManager.GetAllproduct(Products);
+
+        }
+        private void selectOption_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if(((Product)AvatarComboBox.SelectedValue).name != "")
+            {
+                string name = ((Product)AvatarComboBox.SelectedValue).name;
+                string description = Description.Text;
+                var newProduct = new Product(name, description, ProductCategory.cogai);
+                this.Products.Add(newProduct);
+                ProductManager.GetAllproduct(Products);
+
+            }
+        }
+    }
+}
---
+++ Exam_UWP/Models/Product.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Exam_UWP.Models.Product;
+
+namespace Exam_UWP.Models
+{
+   public class Product
+    {
+
+        public enum ProductCategory
+        {
+            bautroi,
+            cogai,
+            khungcanh,
+        }
+
+        public string name { get; set; }
+        public string description { get; set; }
+        public string img { get; set; }
+
+        public Product(string name, string description , ProductCategory category )
+        {
+            name = name;
+            description = description;
+            img = String.Format("/Assets/Images/{0}/{1}.jpg", category,name);
+
+        }
+
+    }
+   public class ProductManager
+    {
+        private static List<Product> getProducts()
+        {
+            var products = new List<Product>();
+            products.Add(new Product("khungcanh","New 100 % " ,ProductCategory.khungcanh));
+            products.Add(new Product("cogai","99 %", ProductCategory.cogai));
+            products.Add(new Product("bautroi","90 % ", ProductCategory.bautroi));
+
+            return products;
+
+        }
+        public static void GetAllproduct(ObservableCollection<Product> products)
+        {
+            var allProduct = getProducts();
+            products.Clear();
+            allProduct.ForEach(p => products.Add(p));
+        }
+
+        public static void addProduct(Product products, ObservableCollection<Product> productList)
+        {
+            var allProduct = getProducts();
+            productList.Clear();
+            allProduct.ForEach(p => productList.Add(p));
+
+        }
+    }
+}
